b253ddf40f20ab0066108f4a1cba981811e42763
kyle-stadler <kyle.stads@gmail.com>
2023-11-13 11:44:12 -0500

    stop tracking picks.txt

--- app.py
+++ app.py
@@ -1,66 +1,63 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from twilio.rest import Client  # Import the Twilio client
+from twilio.rest import Client
 
-# Initialize a headless browser (you can use a different browser if you prefer)
+# Constants
+SCRAPING_URL = 'https://www.oddsshopper.com/odds/shop/?operatortype=sportsbook'
+TWILIO_SID = 'ACbe5f026453cd2028c877428417a3337e'
+TWILIO_AUTH = 'c1bb955c7af60b5b837709897cd87811'
+
+# Initialize a headless browser
 driver = webdriver.Chrome()
+driver.get(SCRAPING_URL)
 
-# Replace with the URL of the website you want to scrape
-url = 'https://www.oddsshopper.com/odds/shop/?operatortype=sportsbook'
-driver.get(url)
-
-# Find the first dynamic <div> element with the specified class
+# The div that I'm looking to scrape is the one that all the data within the odds table shares
 div_element = driver.find_element(
     By.CSS_SELECTOR, 'div.os-table--col.line-col.MuiDataGrid-cell--withRenderer.MuiDataGrid-cell.MuiDataGrid-cell--textLeft.MuiDataGrid-withBorderColor')
 
-# Find the first sportsbook image
+# Finding sportsbook image metadata since it has a different selector than the others
 sportsbook = driver.find_element(
-    By.CSS_SELECTOR, 'img.MuiAvatar-img.css-1hy9t21')  # Fixed the CSS selector
+    By.CSS_SELECTOR, 'img.MuiAvatar-img.css-1hy9t21')
 
-# Your Twilio Account SID and Auth Token
-account_sid = 'ACbe5f026453cd2028c877428417a3337e'
-auth_token = 'c1bb955c7af60b5b837709897cd87811'
-
-# Get the text of the first matching <div> element along with sportsbook info
+# Extracting the text that is within the divs, the text is equal to each cell within the odds table
 parent_div = div_element.find_element(By.XPATH, './parent::div')
 matching_text = parent_div.text
+
+# Splitting the total text into lines so I can adjust what data to include easier
 lines = matching_text.split('\n')
 
-lines.pop(10)
-
 rating = lines[6]
-if int(rating) > 50:
+if int(rating) >= 20:
     sportsbook_name = sportsbook.get_attribute("alt")
-    # Construct the 'pick' variable with all lines except line 11
-    pick_lines = lines[:10] + lines[11:]
-    pick = "\n".join(pick_lines) + f"\nSportsbook: {sportsbook_name}"
+    # Gets rid of the last two lines which is the sportsbook bonus and time it's been up
+    pick_lines = lines[:11]
+    pick = "\n".join(pick_lines) + f"\nSportsbook: {sportsbook_name}\n\n"
+
+    # Create the Twilio client
+    client = Client(TWILIO_SID, TWILIO_AUTH)
 
     # Check if pick is already in the file
-    with open('picks.txt', 'r') as file:
-        existing_content = file.read()
+    with open('picks.txt', 'r', encoding='utf-8') as file:
+        logged_picks = file.read()
 
-        # Split pick and existing_content into lines
-        pick_lines = pick.split('\n')[:3]  # Take the first 3 lines of pick
-        existing_lines = existing_content.split(
-            '\n')[:3]  # Take the first 3 lines
+        # Here i'm only checking if the first 4 lines of each pick are already logged (League, Team/player, Time/date, Bet Type)
+        pick_lines = pick.split('\n')[:4]  # Take the first 4 lines of pick
 
-        first_three_lines = '\n'.join(existing_lines)
+        pick_header = '\n'.join(pick_lines)
 
-        if first_three_lines not in existing_content:
+        if pick_header not in logged_picks:
             # Append pick to the file
-            with open('picks.txt', 'a') as file:
+            with open('picks.txt', 'a', encoding='utf-8') as file:
                 file.write(pick)
-                print("pick added to the file.")
 
             # Send a text using Twilio
-            # message = client.messages.create(
-            #     from_='+18339322565',
-            #     body=pick,
-            #     to='+14405032055'
-            # )
-            # print(f"Text message sent. SID: {message.sid}")
+            # if int(rating) >= 50:
+            #     message = client.messages.create(
+            #         from_='+18339322565',
+            #         body=pick,
+            #         to='+14405032055'
+            #     )
+            #     print(f"Text message sent. SID: {message.sid}")
 
 # Close the browser
 driver.quit()
